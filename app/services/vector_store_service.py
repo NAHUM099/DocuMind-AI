21d17e490a87f50c895eacc05fa6378bd4554f0b
NAHUM099 <131315229+NAHUM099@users.noreply.github.com>
2026-07-25 12:38:28 -0500

    fix:se reemplaza el vectorstore al subir nuevo documento

--- app/services/vector_store_service.py
+++ app/services/vector_store_service.py
@@ -8,14 +8,18 @@ class VectorStoreService:
 
     def save(self, chunks: list[str], embeddings) -> None:
 
+        path = Path(settings.VECTOR_STORE_PATH)
+
+        if path.exists():
+            for file in path.iterdir():
+                file.unlink()
+        else:
+            path.mkdir(parents=True, exist_ok=True)
+
         vector_store = FAISS.from_texts(
             texts=chunks,
             embedding=embeddings
         )
-
-        path = Path(settings.VECTOR_STORE_PATH)
-
-        path.mkdir(parents=True, exist_ok=True)
 
         vector_store.save_local(str(path))
 
